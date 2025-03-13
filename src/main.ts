4166633f0e3283a8d48f1f61e1c76191e6941ab5
Nikol_Niven <ntopalusic@gmail.com>
2025-03-13 17:53:35 +0100

    feat: implement weather location search and display functionality - to rechek

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-// src/main.ts
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeatherData,
+} from './utils';
+
+
+const formElm = document.getElementById('weather-form') as HTMLElement;
+
+formElm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const locationInput = document.getElementById('location') as HTMLInputElement;
+  const locationName = locationInput.value;
+
+  console.log(
+    `The user has submitted the form and is searching for a location with this name${locationName}`,
+  );
+
+  getLocation(locationName)
+    .then((locationResponse) => {
+      // Here you should:
+      // 1. Use displayLocation() with the location data
+      displayLocation(locationResponse)
+      // 2. Use getCurrentWeather() to get weather data
+      getCurrentWeather(locationResponse);
+      // 3. Use displayWeatherData() to show the weather
+      displayWeatherData(locationResponse);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,55 @@
-// src/utils.ts
-
 import axios from 'axios';
-import { LocationResponse, Location } from "./types";
+import { LocationResponse, Location, WeatherResponse } from './types';
 
-
-
-export function getLocation(locationName: string): Promise<LocationResponse> {
-    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
-    return axios.get(url).then((response) => response.data);
+export async function getLocation(
+  locationName: string,
+): Promise<LocationResponse> {
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
+  const response = await axios.get(url);
+  console.log(response.data);
+  return response.data;
 }
 
+export async function getCurrentWeather(
+  locationDetails: Location,
+): Promise<WeatherResponse> {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+  axios.get<WeatherResponse>(url).then((response) => response.data);
+    const response_1 = await axios.get<WeatherResponse>(url);
+    return response_1.data;
 
+}
 
+export function displayLocation(locationDetails: Location): void {
+  const locationContainerElem = document.getElementById('location-container');
+
+  if (locationContainerElem !== null) {
+    locationContainerElem.innerHTML = locationDetails.name;
+  }
+
+  const countryElem = document.getElementById('country');
+
+  if (countryElem !== null) {
+    countryElem.innerHTML = locationDetails.country;
+  }
+}
+
+export function displayWeatherData(obj: WeatherResponse): void {
+  const temperatureElm = document.getElementById('temperature') as HTMLElement;
+  const temperature = obj.current_weather.temperature;
+  const temperatureUnits = obj.current_weather_units.temperature;
+  temperatureElm.innerText = `Temperature: ${temperature} ${temperatureUnits}`;
+
+  const windSpeedElm = document.getElementById('windspeed') as HTMLElement;
+  const windSpeed = obj.current_weather.windspeed;
+  const windSpeedUnits = obj.current_weather_units.windspeed;
+  windSpeedElm.innerText = `Wind Speed: ${windSpeed} ${windSpeedUnits}`;
+
+  const windDirectionElm = document.getElementById(
+    'winddirection',
+  ) as HTMLElement;
+
+  const windDirection = obj.current_weather.winddirection;
+  const windDirectionUnits = obj.current_weather_units.winddirection;
+  windDirectionElm.innerText = `Wind Direction: ${windDirection} ${windDirectionUnits}`;
+}
